5d6925d2ad8a7f1b81d73aee5017f2f773695e2c
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-03-30 23:01:01 -0300

    home

---
+++ front/src/modules/home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutMe from "./about-me";
+import Technologies from "./technologies";
+
+function Home() {
+  return (
+    <div>
+      <AboutMe />
+      <Technologies />
+    </div>
+  );
+}
+
+export default Home;
