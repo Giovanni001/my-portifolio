d8db91fb1703c94f881569ba309171d85ddefcb3
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-03-31 12:22:35 -0300

    skills

--- front/src/modules/home/skills/index.jsx
+++ front/src/modules/home/skills/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Circle from "../../../commom-components/circle";
+import style from "./index.module.css";
+
+import English from "../../../images/skills/english.png";
+import Agility from "../../../images/skills/agility.png";
+import Communication from "../../../images/skills/communication.png";
+import Design from "../../../images/skills/design.png";
+import Office from "../../../images/skills/office.png";
+import Curiosity from "../../../images/skills/curiosity.png";
 
 function Skills() {
-  return <div></div>;
+  const images = [
+    { image: English, text: "Inglês" },
+    { image: Agility, text: "Metodologias Ágeis" },
+    { image: Communication, text: "Comunicativo" },
+    { image: Design, text: "Design" },
+    { image: Office, text: "Pacote Office" },
+    { image: Curiosity, text: "Curioso" },
+  ];
+  return (
+    <>
+      <div className={style.title}>
+        <p>Competências</p>
+      </div>
+
+      <div className={style.container}>
+        {images.map((e) => (
+          <Circle image={e.image} text={e.text} />
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Skills;
