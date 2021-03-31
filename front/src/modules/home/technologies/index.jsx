d53274338ede95c2ac81fa061d7b90e2a10ba21d
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-03-30 23:01:16 -0300

    techs

---
+++ front/src/modules/home/technologies/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Circle from "../../../commom-components/circle";
+
+import ReactImage from "../../../images/technologies/react.png";
+
+import Css from "../../../images/technologies/css.png";
+import JavaScript from "../../../images/technologies/javascript.png";
+import Html from "../../../images/technologies/html.png";
+
+import style from "./index.module.css";
+
+function Technologies() {
+  const technologies = [
+    { image: ReactImage, text: "React" },
+    { image: Css, text: "Css" },
+    { image: JavaScript, text: "JavaScript" },
+    { image: Html, text: "Html" },
+  ];
+
+  return (
+    <>
+      <div className={style.title}>
+        <h1>Tecnologias</h1>
+      </div>
+
+      <div className={style.container}>
+        {technologies.map((e) => (
+          <Circle image={e.image} text={e.text} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Technologies;
