6d72bd9dfba71e38d2e165b4683e9c3fde063f80
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-04-02 22:12:47 -0300

    home

--- front/src/modules/home/index.jsx
+++ front/src/modules/home/index.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import AboutMe from "./about-me";
 import Contact from "./contact";
+import Footer from "./footer";
 import Interests from "./interests";
+import NavBar from "./navbar";
+import Resume from "./resume";
 import Skills from "./skills";
 import Technologies from "./technologies";
 
 function Home() {
   return (
     <div>
+      <NavBar />
       <AboutMe />
       <Technologies />
       <Skills />
       <Interests />
       <Contact />
+      <Resume />
+      <Footer />
     </div>
   );
 }
