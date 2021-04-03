09186651bb7af2c4823ca2c56cd248132fd69b19
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-04-02 22:13:40 -0300

    pdf

---
+++ front/src/modules/home/pdf/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "@react-pdf/renderer";
+
+
+
+const styles = StyleSheet.create({
+
+    /*  pageNumber: {
+         position: "absolute",
+         bottom: pxToPt(30),
+         right: pxToPt(pageHorizontalMargin),
+     }, */
+
+    container: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
+
+export default styles;
