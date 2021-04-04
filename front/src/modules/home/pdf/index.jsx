e72ac1fa81af0e6e0fb128666f59700a437a5ff0
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-04-03 22:01:19 -0300

    pdf

--- front/src/modules/home/pdf/index.jsx
+++ front/src/modules/home/pdf/index.jsx
@@ -1,15 +1,90 @@
 import React from "react";
 import styles from "./styles";
 
-import { Page, View, Document, Image, Text } from "@react-pdf/renderer";
+import { Page, View, Document, Text } from "@react-pdf/renderer";
 
 function Pdf({ data }) {
-  console.log("DATA", data);
   return (
     <Document size="A4">
       <Page>
-        <View style={styles.container}>
-          <Text>{data.name}</Text>
+        <View style={styles.sections}>
+          <View style={styles.container}>
+            {/* Name */}
+            <Text style={styles.mainTitle}>{data.name}</Text>
+          </View>
+          {/* Professional Objectives */}
+          <View style={styles.margimBottomDiv}>
+            <Text style={styles.title}>Objetivos Profissionais </Text>
+            <Text style={styles.description1}>
+              {data.professionalObjectives}
+            </Text>
+          </View>
+          {/* Professional 1 Historic  */}
+          <View style={styles.margimBottomDiv}>
+            <Text style={styles.title}>Historico Profissional </Text>
+            <Text style={styles.description1}>
+              {data.professionalHistoric1.occupation}
+            </Text>
+            <Text style={styles.description2}>
+              {data.professionalHistoric1.companyName}
+            </Text>
+            <Text style={styles.description2}>
+              {data.professionalHistoric1.description}
+            </Text>
+          </View>
+          {/*  Professional 2 Historic */}
+          <View style={styles.margimBottomDiv}>
+            <Text style={styles.title}>Historio Profissional </Text>
+            <Text style={styles.description1}>
+              {data.professionalHistoric2.occupation}
+            </Text>
+            <Text style={styles.description2}>
+              {data.professionalHistoric2.companyName}
+            </Text>
+            <Text style={styles.description2}>
+              {data.professionalHistoric2.description}
+            </Text>
+          </View>
+          {/* Education Historic 1 */}
+          <View style={styles.margimBottomDiv}>
+            <Text style={styles.title}>Historio Escolar </Text>
+            <Text style={styles.description1}>
+              {data.educationHistoric1.institutionName}
+            </Text>
+            <Text style={styles.description2}>
+              {data.educationHistoric1.description}
+            </Text>
+          </View>
+          {/* Education Historic 2 */}
+          <View style={styles.margimBottomDiv}>
+            <Text style={styles.title}>Historio Escolar </Text>
+            <Text style={styles.description1}>
+              {data.educationHistoric2.institutionName}
+            </Text>
+            <Text style={styles.description2}>
+              {data.educationHistoric2.description}
+            </Text>
+          </View>
+          {/* Habilities habilities*/}
+          <View style={styles.margimBottomDiv}>
+            <Text style={styles.title}>Habilidades e Competencias</Text>
+            <Text style={styles.description2}>{data.habilities.one}</Text>
+            <Text style={styles.description2}>{data.habilities.two}</Text>
+            <Text style={styles.description2}>{data.habilities.three}</Text>
+            <Text style={styles.description2}>{data.habilities.four}</Text>
+            <Text style={styles.description2}>{data.habilities.five}</Text>
+            <Text style={styles.description2}>{data.habilities.six}</Text>
+            <Text style={styles.description2}>{data.habilities.seven}</Text>
+          </View>
+          {/* Contact */}
+          <View>
+            <Text style={styles.title}>Contato</Text>
+            <Text style={styles.description2}>{data.contact.firstNumber}</Text>
+            <Text style={styles.description2}>{data.contact.secondNumber}</Text>
+            <Text style={styles.description2}>{data.contact.email}</Text>
+            <Text style={styles.description2}>{data.contact.street}</Text>
+            <Text style={styles.description2}>{data.contact.neighborhood}</Text>
+          </View>
         </View>
       </Page>
     </Document>
--- front/src/modules/home/pdf/styles.js
+++ front/src/modules/home/pdf/styles.js
@@ -1,6 +1,13 @@
-import { StyleSheet } from "@react-pdf/renderer";
+import { StyleSheet, Font } from "@react-pdf/renderer";
+import font1 from '../../../fonts/Roboto-Condensed/RobotoCondensed-Bold.ttf'
+import font2 from '../../../fonts/Roboto-Condensed/RobotoCondensed-Regular.ttf'
 
 
+const pxToPt = (numberInPx) => numberInPx * 0.75;
+
+Font.register({ family: 'RobotoBold', src: font1 });
+
+Font.register({ family: 'RobotoRegular', src: font2 })
 
 const styles = StyleSheet.create({
 
@@ -13,7 +20,38 @@ const styles = StyleSheet.create({
     container: {
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        marginBottom: pxToPt(25),
+        fontFamily: "RobotoBold",
+    },
+
+    mainTitle: {
+        fontSize: pxToPt(30),
+        color: "#0db2ff"
+    },
+
+    title: {
+        fontSize: pxToPt(24),
+        color: "#0db2ff",
+        fontFamily: "RobotoBold",
+        marginBottom: pxToPt(15)
+    },
+
+    description1: {
+        fontSize: pxToPt(20),
+        fontFamily: "RobotoRegular"
+    },
+    description2: {
+        fontSize: pxToPt(18),
+        fontFamily: "RobotoRegular",
+        color: "#656565"
+    },
+    sections: {
+        marginLeft: pxToPt(20),
+        marginRight: pxToPt(20)
+    },
+    margimBottomDiv: {
+        marginBottom: pxToPt(20)
     }
 });
 
