11a0f7640402dcf903015e0c853cf4f9c87fffb3
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-04-08 09:54:14 -0300

    circle

--- front/src/commom-components/circle/index.jsx
+++ front/src/commom-components/circle/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 import style from "./index.module.css";
 
-function Circle({ image, text, onClick }) {
+function Circle({ image, text, onClick, animation }) {
   return (
     <div className={style.container}>
       <div className={style.divImage}>
         <img src={image} alt="" className={style.image} />
       </div>
-      <p onClick={onClick} className={style.textAnimation}>
+      <p
+        onClick={onClick}
+        className={animation ? style.textAnimation : style.textNormal}
+      >
         {text}
       </p>
     </div>
