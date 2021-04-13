abe7ac7a13ec6d54acf570f0372214a586179b31
Giovanni <giovanni.machado@fatec.sp.gov.br>
2021-04-12 21:15:30 -0300

    about me

--- front/src/modules/home/about-me/index.jsx
+++ front/src/modules/home/about-me/index.jsx
@@ -10,7 +10,7 @@ const { DateTime } = require("luxon");
 function AboutMe() {
   let dt = DateTime.now();
   let f = { month: "long", day: "numeric" };
-  let date = dt.setLocale("pt-BR").toLocaleString(f); //=> 'September 14'
+  let date = dt.setLocale("pt-BR").toLocaleString(f);
   let hour = dt.hour;
   let minute = dt.minute;
   let year = dt.year;
@@ -27,7 +27,8 @@ function AboutMe() {
         </div>
         <div className={style.description}>
           <p className={style.descriptionStyle}>
-            Jovem profissional com uma paixão enorme pela área de Tecnologia
+            Jovem profissional com uma paixão enorme em aprender coisas novas e
+            desafiadoras
           </p>
         </div>
         <div className={style.containerPresentationLetter}>
@@ -39,20 +40,26 @@ function AboutMe() {
             <p
               className={style.data}
             >{`${date} de ${year} - ${hour}:${minute}`}</p>
-            <p className={style.letterText}>
+            <p className={style.letterText1}>
               Olá, Me chamo Giovanni Manganotti, tenho 20 anos e moro em
               Jundiaí. Sou um jovem estudante na area de Tecnologia, atualmente
-              cursando Analise e Desenvolvimento de Sistemas na Fatec de
-              Jundiaí. Desde de pequeno sempre fui curioso, sempre buscando
-              aprender coisas novas cada vez mais e mais, e ao crescer isso nao
-              mudou.Estou sempre evoluindo e aprendendo a cada dia mais seja com
-              meus acertos ou com meus erros.
+              cursando Análise e Desenvolvimento de Sistemas na Fatec de
+              Jundiaí. Desde de pequeno sempre fui uma pessoa curiosa, sempre
+              buscando aprender coisas novas e buscar desafios contantes.
+            </p>
+            <p className={style.letterText2}>
+              O meu maior diferencial é a minha persistência e a minha
+              capacidade de sempre dar o meu melhor em tudo o que faço, desde as
+              coisas mais simples até as mais complexas, mesmo não tendo
+              conhecimento em algo em um primeiro momento, eu busco aprender e
+              me informar. Sempre realizando muito networking com o objetivo de
+              fazer novos amigos e aprender coisas novas.
             </p>
             <p className={style.letterTitle}>MEU PROPÓSITO</p>
             <div className={style.hr}>
               <hr className={style.styleHr} />
             </div>
-            <p className={style.letterText}>
+            <p className={style.letterText2}>
               Tenho como propósito, ajudar o maior numero de pessoas durante a
               minha caminhada, deixar o melhor que tenho de mim com elas para
               que um dia elas se lembrem quem foi Giovanni Manganotti Ribeiro
